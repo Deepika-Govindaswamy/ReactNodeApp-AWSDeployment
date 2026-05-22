58e08a4441ac8f17e725a68d5e94a445a1175929
Deepika Govindaswamy <deepikagovindaswamy@gmail.com>
2026-05-22 13:19:08 +0100

    add cloudfront domain

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export default function TaskManager() {
   }, [editingId]);
 
   useEffect(() => {
-  fetch("/tasks")
+  fetch("https://d1isxs8hczjncc.cloudfront.net/tasks")
     .then((res) => res.json())
     .then((data: Task[]) => {
       setTasks(data);
