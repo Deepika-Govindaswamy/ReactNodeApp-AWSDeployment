3e69615ee4a81896c6f3f7e06a4f1b2936b80bcf
Deepika Govindaswamy <deepikagovindaswamy@gmail.com>
2026-05-22 12:13:09 +0100

    restructure index.js

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ app.use(cors())
 
 const { connectToDatabase } = require('./database');
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
 app.get("/tasks", async (req, res) => {
 
     try {
-
         const db = await connectToDatabase();
-
         const tasks = await db.collection("test-node-apps").find({}).toArray();
-
         res.json(tasks);
 
     } catch (error) {
-
         console.error(error);
-
         res.status(500).json({message: "Error fetching tasks"});
     }
 });
@@ -36,3 +35,4 @@ app.listen (port, () => {
 
     console.log (`Server started on ${port}`)
 })
+
